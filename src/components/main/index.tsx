ae4997fcff5f5e37ea12ca94dc9dd9a41713d748
abobi <dekimiq@gmail.com>
2024-10-09 22:49:37 +0300

    swapped out the form and the text

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -31,11 +31,11 @@ function SecondBlock() {
         <AnimatedSection>
           <div className="min-h-screen flex w-full items-center bg-black text-white adaptive-block">
             <div className="flex flex-col items-start w-1/2 p-10 h-1/2 adaptive-subblock">
-              <ContactForm />
-              </div>
-              <div className="flex flex-col items-start w-1/2 p-10 h-1/2 adaptive-subblock">
                 <h2 className="text-lg font-bold">Lorem ipsum</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vulputate odio risus, et consectetur justo sollicitudin ac. Sed nec sodales urna. Aliquam eu tortor eros. Morbi dapibus nisi ornare, ultricies nisi sit amet, venenatis sem. Fusce vel suscipit sem. Quisque auctor neque et turpis dapibus, nec hendrerit tortor fringilla. Mauris aliquam imperdiet libero sit amet molestie. Nullam risus tellus, elementum id fermentum eget, dapibus vitae sem. Aenean ut auctor dui, ac molestie tellus. Aliquam cursus vestibulum sem. Proin accumsan urna in nunc semper pretium. Etiam rhoncus pellentesque orci quis finibus. Suspendisse vitae sagittis dolor. Duis malesuada libero enim, et aliquam nulla sagittis id. Mauris lobortis, orci non malesuada ultricies, sapien sapien condimentum felis, id mattis nisl leo a augue.</p>
+            </div>
+            <div className="flex flex-col items-start w-1/2 p-10 h-1/2 adaptive-subblock">
+              <ContactForm />
               </div>
           </div>
         </AnimatedSection>
