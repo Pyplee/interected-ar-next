20654faf9eadd2b71da452d2849e4160fb4509b1
abobi <dekimiq@gmail.com>
2024-10-09 19:06:33 +0300

    added declare use client

--- src/components/main/ARViewer.tsx
+++ src/components/main/ARViewer.tsx
@@ -1,3 +1,4 @@
+"use client"
 // @ts-nocheck
 import React from 'react'
 import { ArToolkitProfile, ArToolkitSource, ArToolkitContext, ArMarkerControls} from '@ar-js-org/ar.js/three.js/build/ar-threex.js';
