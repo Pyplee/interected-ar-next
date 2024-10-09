ad2bdb6161c3d85c9ed2719bb7cb2c0762c2bb9e
abobi <dekimiq@gmail.com>
2024-10-09 19:09:22 +0300

    fixed size navigate button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
           priority
         />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center flex-col sm:flex-row justify-center">
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="bg-black text-white rounded-lg p-2"
             href="/main"
           >
             Go to Main page
